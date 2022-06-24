5b532e8142817b504b1be70edcbf2f875064a224
zackn9ne <zackn9ne@gmail.com>
2022-06-24 18:35:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,5 @@
-from python_graphql_client import GraphqlClient
 import os
-import json
-from dotenv import load_dotenv
-load_dotenv()  # take environment variables from .env.
-
-headers = { "Authorization": f"Token {os.environ.get('INPUTS_GITHUB_TOKEN')}" }
-client = GraphqlClient(endpoint="https://api.github.com/graphql", headers=headers)
-
-query = """
-    query dependaBotAlerts($owner: String!, $repo: String!) {
-        repository(owner: $owner, name: $repo) {
-        vulnerabilityAlerts(first: 100) {
-            nodes {
-            createdAt
-            dismissedAt
-            securityVulnerability {
-                package {
-                name
-                ecosystem
-                }
-                advisory {
-                severity
-                summary
-                description
-                notificationsPermalink
-                ghsaId
-                }
-                vulnerableVersionRange
-            }
-            }
-        }
-        }
-    }
-"""
-variables = {
-	"owner": os.environ.get('INPUTS_OWNER'),
-	"repo": os.environ.get('INPUTS_REPO'),
-	"ownerzz": "github",
-	"namezz": "view_component"
-}
+import requests  # noqa We are just importing this to prove the dependency installed correctly
 
 
 def main():
@@ -48,12 +9,6 @@ def main():
 
     print(f"::set-output name=myOutput::{my_output}")
 
-def main2():
-	# Synchronous request
-	data = client.execute(query=query, variables=variables)
-	print(data)  # => {'data': {'country': {'code': 'CA', 'name': 'Canada'}}}
-	with open(os.environ.get('INPUTS_OUTFILE'), "w", encoding="utf-8") as d:
-	    d.write(json.dumps(data.get("data")))
-	
+
 if __name__ == "__main__":
     main()
